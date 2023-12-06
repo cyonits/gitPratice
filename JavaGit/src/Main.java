57dad61dccca033f658be680239ff6ccea3c4ca3
cyonits <cyonitss@gmail.com>
2023-12-06 10:39:10 +0900

    테스트 22

--- JavaGit/src/Main.java
+++ JavaGit/src/Main.java
@@ -1,5 +1,14 @@
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("테스트 1");
+//    }
+//}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("테스트 1");
+        //asdasdasd
+        //asdasdas/asd
+        //as/das/d/as/d
+//        as/doubleas/double/as/das
+        System.out.println("Aaaa");
     }
 }
